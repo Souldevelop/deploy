e4df13c2d32023d99166167a4c5d066e13c5ab1f
Reverseking <mosker@126.com>
2026-05-08 14:18:49 +0800

    chore: 版本升至 v2.2.10，新增 install.ps1 和 .gitattributes

--- deploy_claude.sh
+++ deploy_claude.sh
@@ -68,7 +68,7 @@ readonly GITEE_REPO_BASE="https://gitee.com/reverseking/deploy/raw/master"
 SELF_SOURCE=""
 
 # 脚本版本号（更新时请修改此值）
-readonly SCRIPT_VERSION="2.2.9"
+readonly SCRIPT_VERSION="2.2.10"
 
 # ---------------------------------------------------------------------------
 # APT mirror presets
